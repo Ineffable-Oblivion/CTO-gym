0afc3857d5ba11644512f35c4c32047904d1b633
SaiKrishna <balu4wdle@gmail.com>
2019-01-06 15:16:16 +0530

    Implementation

--- gym_cto/envs/cto_env.py
+++ gym_cto/envs/cto_env.py
@@ -3,8 +3,24 @@ import gym
 class CtoEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self):
-        pass
+    def __init__(self, targets=10, sensorRange=15, updateRate=10, totalSimTime=1500):
+        # general variables in the environment
+        self.runTime = totalSimTime        
+
+        #total number of targets in the simulation
+        self.numTargets = targets 
+
+        #sensor range of the observer
+        self.sensorRange = sensorRange
+
+        #time after which observer takes the decision
+        self.updateRate = updateRate
+
+        self.episodes = self.runTime / self.updateRate
+
+        self.curr_episode = 0
+
+
 
     def step(self, action):
         pass
