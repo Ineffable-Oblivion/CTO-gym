3aadcf776b0938c3dda7b219b0cfb18eb751d330
SaiKrishna <balu4wdle@gmail.com>
2019-01-07 11:54:59 +0530

    Implementation

--- gym_cto/envs/cto_env.py
+++ gym_cto/envs/cto_env.py
@@ -1,11 +1,35 @@
 import gym
 import random
 import numpy as np
+from math import sqrt
 
 class CtoEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
+    """
+    Summary of the initialized the environment variables
+            
+        runTime
+            The total time simulation runs for
+
+        updateRate
+            Time interval between each decision making/action
+
+        episodes
+            Equal to runTime / updaRate
+            Simulation terminates after these many episodes
+
+        gridWidth, gridHeight
+            Dimensions of the 2D arena
+
+        sensorRange
+            The maximum distance between agent and target for the agent to notice
+            the target
+    
+    """
+
     def __init__(self, targets=10, sensorRange=15, updateRate=10, targetMaxStep=100,
+                    targetSpeed=1.0,
                     totalSimTime=1500, gridWidth=150, gridHeight=150):
         # general variables in the environment
         self.runTime = totalSimTime        
@@ -42,6 +66,14 @@ class CtoEnv(gym.Env):
                 self.targetLocations[i][0] = random.uniform(0, self.gridWidth)
                 self.targetLocations[i][1] = random.uniform(0, self.gridHeight)
 
+        #Initialize the agent and ensure it is not on top of other target
+        self.agentPosition = np.array([0.0, 0.0])
+        self.agentPosition[0] = random.uniform(0, self.gridWidth)
+        self.agentPosition[1] = random.uniform(0, self.gridHeight)
+        while not self.acceptable(-1, True):
+            self.agentPosition[0] = random.uniform(0, self.gridWidth)
+            self.agentPosition[1] = random.uniform(0, self.gridHeight)
+
 
         self.episodes = self.runTime / self.updateRate
 
@@ -49,40 +81,29 @@ class CtoEnv(gym.Env):
         self.curr_step = 0
 
 
-        """
-        Summary of the initialized the environment variables
-                
-            runTime
-                The total time simulation runs for
-
-            updateRate
-                Time interval between each decision making/action
-
-            episodes
-                Equal to runTime / updaRate
-                Simulation terminates after these many episodes
-
-            gridWidth, gridHeight
-                Dimensions of the 2D arena
-
-            sensorRange
-                The maximum distance between agent and target for the agent to notice
-                the target
-        
-        """
-
     # Checks whether the two points are at least one unit apart
-    def acceptable(self, index):
-        if index == 0:
-            return True
+    def acceptable(self, index, agent=False):
+        if not agent:
+            if index == 0:
+                return True
+            
+            else:
+                for i in xrange(index):
+                    distance = (self.targetLocations[i][0] - self.targetLocations[index][0])**2
+                    distance += (self.targetLocations[i][1] - self.targetLocations[index][1])**2
+
+                    if sqrt(distance) <= 1:
+                        return False
+
+                return True
         
         else:
-            for i in xrange(index):
-                distance = (self.targetLocations[i][0] - self.targetLocations[index][0])**2
-                distance += (self.targetLocations[i][1] - self.targetLocations[index][1])**2
+            for i, pos in enumerate(self.targetLocations):
+                distance = (self.agentPosition[0] - pos[0])**2
+                distance += (self.agentPosition[1] - pos[1])**2
 
-                if distance <= 1:
-                    return False
+                if sqrt(distance) <= 1:
+                        return False
 
             return True
 
